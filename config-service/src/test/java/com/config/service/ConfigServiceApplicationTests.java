36172fae1d8c4c07f91a92246301f9822f51a459
Hugo Benitez <hugo.boca16@gmail.com>
2026-08-04 16:03:39 -0300

    config

--- config-service/src/test/java/com/config/service/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/config/service/ConfigServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.eureka.server;
+package com.config.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class ConfigServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
