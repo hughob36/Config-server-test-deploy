13b2066779fbe796dc63d1d2966916eb7a751c88
Hugo Benitez <hugo.boca16@gmail.com>
2026-08-04 17:37:08 -0300

    feign client

--- converter-service/src/main/java/com/conveter/service/ConverterServiceApplication.java
+++ converter-service/src/main/java/com/conveter/service/ConverterServiceApplication.java
@@ -2,8 +2,10 @@ package com.conveter.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ConverterServiceApplication {
 
 	public static void main(String[] args) {
