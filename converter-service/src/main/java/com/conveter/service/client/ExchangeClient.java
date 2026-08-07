ad3384798a0fdcb9509e4b7967daffb5663d024f
Hugo Benitez <hugo.boca16@gmail.com>
2026-08-07 14:24:06 -0300

    url excahnge

--- converter-service/src/main/java/com/conveter/service/client/ExchangeClient.java
+++ converter-service/src/main/java/com/conveter/service/client/ExchangeClient.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
 
-@FeignClient(name = "exchange-service")
+//@FeignClient(name = "exchange-service")
+@FeignClient(
+        name = "exchange-service",
+        url = "${EXCHANGE_SERVICE_URL:https://stunning-celebration-production-3657.up.railway.app}"
+)
 public interface ExchangeClient {
 
     @GetMapping("/api/rate")
